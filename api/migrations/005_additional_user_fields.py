298388fd3b2dfedddf4a76d221a2509163855d65
Billy Berger-Bailey <bbergerbailey@gmail.com>
2024-04-16 18:50:18 +0000

    Resolve "Edit users table"

---
+++ api/migrations/005_additional_user_fields.py
@@ -0,0 +1,19 @@
+steps = [
+    [
+        # "Up" SQL statement
+        """
+        ALTER TABLE users
+            ADD first_name VARCHAR(50) NOT NULL,
+            ADD last_name VARCHAR(50) NOT NULL,
+            ADD address VARCHAR(200) NOT NULL,
+            ADD birthday DATE NOT NULL,
+            ADD favorite_team_id INT NOT NULL
+        ;
+        """,
+        # "Down" SQL statement
+        """
+        DROP COLUMN first_name, last_name, address, birthday,
+        favorite_team_id;
+        """
+    ],
+]
