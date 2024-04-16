36f63283548147cccacd5042bf098fc00182b9f9
Billy Berger-Bailey <bbergerbailey@gmail.com>
2024-04-16 18:11:09 +0000

    Teams table created

---
+++ api/migrations/004_teams_table.py
@@ -0,0 +1,27 @@
+steps = [
+    [
+        # "Up" SQL statement
+        """
+        CREATE TABLE teams (
+            id INT PRIMARY KEY NOT NULL,
+            team_name VARCHAR(15) NOT NULL,
+            full_name VARCHAR(50) NOT NULL,
+            color VARCHAR(20) NOT NULL,
+            alternative_color VARCHAR(20) NOT NULL,
+            logo VARCHAR(250) NOT NULL,
+            location VARCHAR(50) NOT NULL,
+            record VARCHAR(8) NOT NULL,
+            standing VARCHAR(50) NOT NULL
+            );
+
+
+
+        """,
+
+        # "Down" SQL statement
+        """
+        DROP TABLE teams;
+        """
+
+    ]
+]
