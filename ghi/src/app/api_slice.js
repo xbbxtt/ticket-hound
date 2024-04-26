5ce4e17311754f39b4626776bf74d2939cf9a80a
Billy Berger-Bailey <bbergerbailey@gmail.com>
2024-04-26 15:41:20 +0000

    react-redux-beginning

---
+++ ghi/src/app/api_slice.js
@@ -0,0 +1,17 @@
+import { createApi, fetchBaseQuery } from @reduxjs/toolkit/query/react
+
+export const mlbApi = createApi({
+    reducerPath: 'mlbApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: import meta.env.VITE_API_HOST
+    }),
+    endpoints: (builder) => ({
+        listmlbTeams: builder.query({
+            query: () => ({
+                url: '/api/teams'
+            })
+        })
+    })
+})
+
+export const { useListMlbTeamsQuery } = mlbApi
