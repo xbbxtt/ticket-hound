6589c366494b0b635a49e75028eced05e60b01f2
Billy Berger-Bailey <bbergerbailey@gmail.com>
2024-04-26 16:07:51 +0000

    Resolve "bug fixes"

--- ghi/src/app/api_slice.js
+++ ghi/src/app/api_slice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 export const mlbApi = createApi({
     reducerPath: 'mlbApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: import meta.env.VITE_API_HOST
+        baseUrl: import.meta.env.VITE_API_HOST
     }),
     endpoints: (builder) => ({
         listmlbTeams: builder.query({
