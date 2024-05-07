5738d2da1f51811805daaac6081a7c3f1db5d589
nathan <nathan>
2024-05-07 12:55:47 -0400

    Styles the input form box component

--- ghi/src/components/InputBox.jsx
+++ ghi/src/components/InputBox.jsx
@@ -1,18 +1,27 @@
-export default function InputBox({type, name, value, onChange, placeholder, title}) {
+export default function InputBox({
+    type,
+    name,
+    value,
+    onChange,
+    placeholder,
+    title,
+}) {
     return (
-        <div>
-            <label htmlFor={name} className="">
+        <div className="row">
+            <label htmlFor={name} className="form-label mb-2">
                 {title}
             </label>
-            <input
-                id={name}
-                type={type}
-                name={name}
-                value={value}
-                onChange={onChange}
-                placeholder={placeholder}
-            />
+            <div className="input-group mb-3">
+                <input
+                    className="form-control mb-2"
+                    id={name}
+                    type={type}
+                    name={name}
+                    value={value}
+                    onChange={onChange}
+                    placeholder={placeholder}
+                />
+            </div>
         </div>
     )
 }
-
