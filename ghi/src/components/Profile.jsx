74939ead8a6d573d0d66d5a52a49bfc938ab4f66
Nahom Zeleke <nahomzeleke@gmail.com>
2024-05-07 18:30:59 -0400

    added css to the profile page

--- ghi/src/components/Profile.jsx
+++ ghi/src/components/Profile.jsx
@@ -6,7 +6,6 @@ export default function Profile() {
     const { data: user, isLoading: isLoadingUser } =
         mlbApi.useAuthenticateQuery()
 
-
     const [deleteUser, deleteUserStatus] = mlbApi.useDeleteUserMutation()
     const [error, setErrorMessage] = useState('')
 
@@ -15,7 +14,6 @@ export default function Profile() {
         user?.favorite_team_id
     )
     const [teamName, setTeamName] = useState()
-
 
     useEffect(() => {
         if (!user && !isLoadingUser) {
@@ -31,28 +29,59 @@ export default function Profile() {
     const handleDelete = (e) => {
         e.preventDefault()
         deleteUser()
-
     }
 
-    useEffect (() => {
-
+    useEffect(() => {
         if (deleteUserStatus.isSuccess) {
             navigate('/')
-
         } else if (deleteUserStatus.isError) {
             setErrorMessage('')
-
         }
-
-
     }, [deleteUserStatus, navigate])
 
     if (isLoadingUser) {
         return <div>Loading...</div>
     }
     return (
-        <div>
-            <table>
+        <div className="container-fluid mb-3">
+            <div className="offset-3 col-6" id="profile-card">
+                <div className="shadow p-4 mt-4">
+                    <p>Username: {user?.username}</p>
+                    <p>
+                        Name: {user?.first_name} {user?.last_name}
+                    </p>
+                    <p className="lexend-deca-uniquifier">
+                        Address: {user?.address}
+                    </p>
+                    <p>Birthday: {user?.birthday}</p>
+                    <p>Favorite Team: {teamName?.full_name}</p>
+                    {teamName && (
+                        <img
+                            src={teamName.logo}
+                            alt="Favorite team's logo"
+                            width="200"
+                        />
+                    )}
+                    <div>
+                        <button
+                            type="button"
+                            class="btn btn-secondary mx-2 btn-lg"
+                            onClick={() => navigate('/edit')}
+                        >
+                            Edit Profile
+                        </button>
+                        <button
+                            type="button"
+                            class="btn btn-danger mx-2 btn-lg"
+                            onClick={handleDelete}
+                        >
+                            Delete Profile
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+            {/* <table>
                 <thead>
                     <tr>
                         <th>Profile</th>
@@ -83,7 +112,26 @@ export default function Profile() {
                 </tbody>
             </table>
             <button onClick={() => navigate('/edit')}>Edit Profile</button>
-            <button onClick={handleDelete}>Delete Profile</button>
+            <button onClick={handleDelete}>Delete Profile</button> */}
         </div>
     )
+}
+
+{
+    /* <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">An item</li>
+    <li class="list-group-item">A second item</li>
+    <li class="list-group-item">A third item</li>
+  </ul>
+  <div class="card-body">
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div> */
 }
