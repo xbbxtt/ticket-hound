f950d23ac780fce3f7013ed6ea2d533f9b9866e3
Nahom Zeleke <nahomzeleke@gmail.com>
2024-05-08 15:30:02 -0400

    safe save

--- ghi/src/components/Nav.jsx
+++ ghi/src/components/Nav.jsx
@@ -24,13 +24,10 @@ export default function Nav() {
         mlbApi.useAuthenticateQuery()
 
     return (
-        <nav
-            className="navbar navbar-expand-lg navbar bg-success"
-            id="navigation-container"
-        >
-            <div className="container-fluid">
-                <div>
-                    <ul className="navbar-nav me-auto mb-2">
+        <nav className="" id="navigation-container">
+            <div className="container-fluid text-center">
+                <div class="row align-items-start">
+                    <div class="col-9">
                         <li className="nav-item nav-link" role="button">
                             <NavLink className="navbar-brand" to={'/'}>
                                 {' '}
@@ -41,20 +38,8 @@ export default function Nav() {
                                 />
                             </NavLink>
                         </li>
-                        {user && (
-                            <li className="nav-item mb-1" role="button">
-                                <NavLink className="nav-link" to={'/games'}>
-                                    Games
-                                </NavLink>
-                            </li>
-                        )}
-                        {user && (
-                            <li className="nav-item mb-1" role="button">
-                                <NavLink className="nav-link" to={'/profile'}>
-                                    Profile
-                                </NavLink>
-                            </li>
-                        )}
+                    </div>
+                    <div class="col-3">
                         {user && (
                             <div
                                 class="d-grid gap-2 d-md-flex justify-content-md-end"
@@ -71,23 +56,75 @@ export default function Nav() {
                                 </li>
                             </div>
                         )}
-                        {!user && (
-                            <li className="nav-item mb-1" role="button">
-                                <NavLink className="nav-link" to={'/signup'}>
-                                    Sign Up
-                                </NavLink>
-                            </li>
-                        )}
-                        {!user && (
-                            <li className="nav-item mb-1" role="button">
-                                <NavLink className="nav-link" to={'/signin'}>
-                                    Log in{' '}
-                                </NavLink>
-                            </li>
-                        )}
-                    </ul>
+                    </div>
                 </div>
             </div>
         </nav>
     )
 }
+
+// ;<nav
+//     className="navbar navbar-expand-lg navbar bg-success"
+//     id="navigation-container"
+// >
+//     <div className="container-fluid">
+//         <div>
+//             <ul className="navbar-nav me-auto mb-2">
+//                 <li className="nav-item nav-link" role="button">
+//                     <NavLink className="navbar-brand" to={'/'}>
+//                         {' '}
+//                         <img
+//                             src="https://i.postimg.cc/kXPX3pZw/A577-C781-F0-A8-43-EF-9-C9-B-B01687700-DDC-4-5005-c.jpg"
+//                             width="65"
+//                             height="50"
+//                         />
+//                     </NavLink>
+//                 </li>
+//                 {user && (
+//                     <li className="nav-item mb-1" role="button">
+//                         <NavLink className="nav-link" to={'/games'}>
+//                             Games
+//                         </NavLink>
+//                     </li>
+//                 )}
+//                 {user && (
+//                     <li className="nav-item mb-1" role="button">
+//                         <NavLink className="nav-link" to={'/profile'}>
+//                             Profile
+//                         </NavLink>
+//                     </li>
+//                 )}
+//                 {user && (
+//                     <div
+//                         class="d-grid gap-2 d-md-flex justify-content-md-end"
+//                         id="signout-button"
+//                     >
+//                         <li className="nav-item mb-3" role="button">
+//                             <button
+//                                 type="button"
+//                                 class="btn btn-primary btn-md"
+//                                 onClick={handleLogOut}
+//                             >
+//                                 Logout
+//                             </button>
+//                         </li>
+//                     </div>
+//                 )}
+//                 {!user && (
+//                     <li className="nav-item mb-1" role="button">
+//                         <NavLink className="nav-link" to={'/signup'}>
+//                             Sign Up
+//                         </NavLink>
+//                     </li>
+//                 )}
+//                 {!user && (
+//                     <li className="nav-item mb-1" role="button">
+//                         <NavLink className="nav-link" to={'/signin'}>
+//                             Log in{' '}
+//                         </NavLink>
+//                     </li>
+//                 )}
+//             </ul>
+//         </div>
+//     </div>
+// </nav>
