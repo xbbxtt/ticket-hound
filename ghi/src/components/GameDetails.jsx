bc3b19db14bcc34b611580daac665c21b72787a7
Billy Berger-Bailey <bbergerbailey@gmail.com>
2024-05-15 17:35:54 +0000

    Resolve "39-display-standings"

--- ghi/src/components/GameDetails.jsx
+++ ghi/src/components/GameDetails.jsx
@@ -1,8 +1,9 @@
 import { mlbApi } from '../app/apiSlice'
-import { useLocation } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 import GetSeatgeekTickets from './GetSeatgeekTickets'
 import GetVividseatsTickets from './GetVividseatsTickets'
 import GetTickpickTickets from './GetTickpickTickets'
+import { useEffect, useState } from 'react'
 
 export default function GameDetails() {
     // Using the game's id, this component will make
@@ -10,9 +11,41 @@ export default function GameDetails() {
     const location = useLocation()
     const id = location.state.id
 
+    const navigate = useNavigate()
+
     const { data: gameData, isLoading: gameIsLoading } =
         mlbApi.useGameDetailsQuery(id)
 
+    const [homeTeam, setHomeTeam] = useState('')
+    const [awayTeam, setAwayTeam] = useState('')
+    const [recordData, setRecordData] = useState({})
+
+    const [trigger, result] = mlbApi.useLazyRecordDetailsQuery()
+
+    useEffect(() => {
+        if (!gameData && !gameIsLoading) {
+            navigate('/')
+        } else if (gameData && !gameIsLoading) {
+            setHomeTeam(gameData.home_team)
+            setAwayTeam(gameData.away_team)
+            if (homeTeam && awayTeam) {
+                trigger({ homeTeam, awayTeam })
+            }
+        }
+    }, [
+        gameData,
+        gameIsLoading,
+        navigate,
+        trigger,
+        setHomeTeam,
+        setAwayTeam,
+        homeTeam,
+        awayTeam,
+    ])
+
+    useEffect(() => {
+        if (result.isSuccess) setRecordData(result.data)
+    }, [result])
     if (gameIsLoading) return <div>Loading...</div>
 
     const date = new Date(gameData.game_date)
@@ -56,12 +89,22 @@ export default function GameDetails() {
                         </div>
                     </div>
                     <div className="col-3">
-                        <div className="card mb-1 row-3">
+                        <div className="card mb-1 row-3 russo-one-regular">
                             <div>
                                 <div>{gameData.location}</div>
                                 <div>{formattedTime}</div>
                                 <div>{date.toDateString()}</div>
                             </div>
+                        </div>
+                        <div className="card mb-1 row-3 russo-one-regular">
+                            <div>{gameData.away_team}</div>
+                            <div>{recordData.away_record}</div>
+                            <div>{recordData.away_standings}</div>
+                        </div>
+                        <div className="card mb-1 row-3 russo-one-regular">
+                            <div>{gameData.home_team}</div>
+                            <div>{recordData.home_record}</div>
+                            <div>{recordData.home_standings}</div>
                         </div>
                     </div>
                 </div>
